394935940904e233970a0312052f7acc463389d4
merikurisalad <116539775+merikurisalad@users.noreply.github.com>
2022-12-30 03:25:10 -0800

    API request and return string
    Added express.js to project (under server folder), added button that makes API call and returns string "Hello World!"

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+  const [joke, setJoke] = useState("");
+
+  const getJoke = () => {
+    Axios.get("https://official-joke-api.appspot.com/random_joke").then(
+      (response) => {
+        //setJoke(response.data.setup + " ... " + response.data.punchline); // displays random joke
+        setJoke("Hello World!") // default response?
+        }
+    );
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Test <button onClick={getJoke}>Click</button>
+        {joke}
       </header>
     </div>
   );
