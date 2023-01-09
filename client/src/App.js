6151a07b58d62879f86152cdc4daba8eb8fa8765
merikurisalad <116539775+merikurisalad@users.noreply.github.com>
2023-01-09 07:50:36 -0800

    Created database
    Connected database to frontend, created table for Courses (columns include course code & name)

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [courseCode, setCourseCode] = useState('')
+  const [courseName, setCourseName] = useState('')
+  const [courseList, setCourseList] = useState([])
+
+  useEffect(()=> {
+    Axios.get('http://localhost:4000/api/get').then((response)=> {
+      setCourseList(response.data)
+      console.log(response.data)
+    })
+  }, [])
+
+  const submitCourse = () => {
+    Axios.post('http://localhost:4000/api/insert', {
+      courseCode: courseCode,
+      courseName: courseName,
+    }).then(() => {
+      alert("Successful insert")
+    })
+  }
+
+  return (
+    <div className="App">
+      <h1>Course Scores App</h1>
+
+      <div className="form">
+        <label>Course Code:</label>
+        <input type="text" name="courseCode" onChange={(e)=> {
+          setCourseCode(e.target.value)
+        }}/>
+        <label>Course Name:</label>
+        <input type="text" name="courseName" onChange={(e)=> {
+          setCourseName(e.target.value)
+        }}/>
+
+        <button onClick={submitCourse}>Submit</button>
+
+        {courseList.map((val)=> {
+          return ( 
+            <h2>
+              Course Code: {val.courseCode} |  Course Name: {val.courseName}
+            </h2>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+/* function App() {
   const [joke, setJoke] = useState("");
 
   const getJoke = () => {
@@ -22,6 +72,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const port = 4000
+const mysql = require('mysql2')
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'CourseScoresDB',
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/api/get', (req, res)=> {
+  const sqlSelect = "SELECT * FROM Courses"
+  db.query(sqlSelect, (err, result) => {
+    res.send(result)
+  })
+})
+
+app.post('/api/insert', (req, res)=> {
+  
+  const courseCode = req.body.courseCode
+  const courseName = req.body.courseName
+  
+  const sqlInsert = "INSERT INTO Courses (courseCode, courseName) VALUES (?,?)"
+  db.query(sqlInsert, [courseCode, courseName], (err, result)=> {
+    console.log(result)
+  })
+})
+
+/* app.get('/', (req, res) => {
+  const sqlInsert = "INSERT INTO Courses (courseCode, courseName) VALUES ('CPSC 213', 'Introduction to Computer Systems');"
+  db.query(sqlInsert, (err, result) => {
+    res.send('HELLO WORLD!')
+  })
+})
+ */
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
